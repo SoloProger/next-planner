eea3c6ab7a0ca6fd1e0bc0ed53b7daf8fe9b627d
serafimow <aserafimow@gmail.com>
2024-03-05 09:56:58 +0300

    Перевел приложение на React

--- src/app/routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { SignInComponent } from '../pages/sign-in/sign-in.component';
-import { SignUpComponent } from './../pages/sign-up/sign-up.component';
-
-export const routes: Routes = [
-  { path: 'auth/sign-in', component: SignInComponent },
-  { path: 'auth/sign-up', component: SignUpComponent },
-];
--- src/shared/ui/button/button.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-button',
-  templateUrl: './button.component.html',
-  styleUrl: './button.component.scss',
-  standalone: true,
-})
-export class ButtonComponent {}
