2aa3611c46c489f738d6d2c9335894e82256bac6
andreisolo <asolonnikow@gmail.com>
2024-06-07 16:35:31 +0300

    FRONTEND-8: Вынес провайдеры

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -10,56 +10,59 @@ import {
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../widgets/navbar";
 import { Sidebar } from "../widgets/sidebar";
+import CustomQueryClientProvider from "./providers/CustomQueryClientProvider";
 
 function App() {
   const [isOpenSidebar, setOpenSidebar] = useState(true);
   const [isShow, setShow] = useState(false);
 
   return (
-    <ChakraProvider>
-      <Navbar openSidebar={() => setOpenSidebar(!isOpenSidebar)} />
-      <main className="flex gap-24 relative h-vh-100">
-        <Sidebar isOpen={isOpenSidebar} />
-        <Outlet />
-        <div
-          className="absolute b-135 r-80 bg-orange p-16 border-circle pointer"
-          onClick={() => setShow(!isShow)}
-        >
-          <FaPlus size="24px" />
-        </div>
+    <CustomQueryClientProvider>
+      <ChakraProvider>
+        <Navbar openSidebar={() => setOpenSidebar(!isOpenSidebar)} />
+        <main className="flex gap-24 relative h-vh-100">
+          <Sidebar isOpen={isOpenSidebar} />
+          <Outlet />
+          <div
+            className="absolute b-135 r-80 bg-orange p-16 border-circle pointer"
+            onClick={() => setShow(!isShow)}
+          >
+            <FaPlus size="24px" />
+          </div>
 
-        <>
-          <div
-            className={`trans-1 absolute b-90 r-135 bg-orange p-16 border-circle pointer ${
-              isShow ? "opacity" : "opacity-none"
-            }`}
-          >
-            <FaList />
-          </div>
-          <div
-            className={`trans-1  absolute b-140 r-150 bg-orange p-16 border-circle pointer ${
-              isShow ? "opacity" : "opacity-none"
-            }`}
-          >
-            <FaChartSimple />
-          </div>
-          <div
-            className={`trans-1  absolute b-190 r-130 bg-orange p-16 border-circle pointer ${
-              isShow ? "opacity" : "opacity-none"
-            }`}
-          >
-            <FaFileInvoiceDollar />
-          </div>
-          <div
-            className={`trans-1  absolute b-220 r-85 bg-orange p-16 border-circle pointer ${
-              isShow ? "opacity" : "opacity-none"
-            }`}
-          >
-            <FaCreditCard />
-          </div>
-        </>
-      </main>
-    </ChakraProvider>
+          <>
+            <div
+              className={`trans-1 absolute b-90 r-135 bg-orange p-16 border-circle pointer ${
+                isShow ? "opacity" : "opacity-none"
+              }`}
+            >
+              <FaList />
+            </div>
+            <div
+              className={`trans-1  absolute b-140 r-150 bg-orange p-16 border-circle pointer ${
+                isShow ? "opacity" : "opacity-none"
+              }`}
+            >
+              <FaChartSimple />
+            </div>
+            <div
+              className={`trans-1  absolute b-190 r-130 bg-orange p-16 border-circle pointer ${
+                isShow ? "opacity" : "opacity-none"
+              }`}
+            >
+              <FaFileInvoiceDollar />
+            </div>
+            <div
+              className={`trans-1  absolute b-220 r-85 bg-orange p-16 border-circle pointer ${
+                isShow ? "opacity" : "opacity-none"
+              }`}
+            >
+              <FaCreditCard />
+            </div>
+          </>
+        </main>
+      </ChakraProvider>
+    </CustomQueryClientProvider>
   );
 }
 
