31b41ea8ab78a9541ab20aa295987ae99921b61f
andreisolo <asolonnikow@gmail.com>
2024-06-10 15:54:55 +0300

    DEV: hotfix zustand

--- frontend/src/entities/invoice/index.ts
+++ frontend/src/entities/invoice/index.ts
@@ -1,7 +1,7 @@
 import { getInvoices } from "./api/getInvoices";
-import { useInvoiceStore } from "./model/store/invoiceStore";
+
 import type { Invoice } from "./model/types/Invoice";
 import { InvoiceType } from "./model/types/InvoiceType";
 import InvoiceCard from "./ui/InvoiceCard";
 
-export { getInvoices, Invoice, InvoiceCard, InvoiceType, useInvoiceStore };
+export { getInvoices, Invoice, InvoiceCard, InvoiceType };
--- frontend/src/entities/invoice/model/store/invoiceStore.ts
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-import { InvoiceStore } from "../types/InvoiceStore";
-
-export const useInvoiceStore = create<InvoiceStore>()((set) => ({
-  invoices: [],
-  addInvoiceToStore: (invoice) =>
-    set((state) => ({ invoices: [...state.invoices, invoice] })),
-}));
