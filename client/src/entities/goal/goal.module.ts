9eaf2998f0386f1c2d69404e204517796ce18ffa
andreisolonnikov <asolonnikow@gmail.com>
2024-11-23 09:42:57 +0300

    dev: Изменил внешний вид карточки цели

--- client/src/entities/goal/goal.module.ts
+++ client/src/entities/goal/goal.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { GoalCardComponent } from './ui/goal-card/goal-card.component';
 import { GoalGridComponent } from './ui/goal-grid/goal-grid.component';
 import { CommonModule } from '@angular/common';
+import { ProgressBarComponent } from '../../shared/ui/pogress-bar/progress-bar.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ProgressBarComponent],
   exports: [GoalGridComponent],
   declarations: [GoalGridComponent, GoalCardComponent],
   providers: [],
--- client/src/entities/goal/ui/goal-card/goal-card.component.ts
+++ client/src/entities/goal/ui/goal-card/goal-card.component.ts
@@ -1,15 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-goal-card',
   templateUrl: 'goal-card.component.html',
 })
 export class GoalCardComponent {
-  @Input({ required: true }) name!: string;
-
-  @Input({ required: true }) description!: string;
-
-  @Input({ required: true }) currentAmount!: number;
-
-  @Input({ required: true }) totalAmount!: number;
+  public name = input.required<string>();
+  public description = input.required<string>();
+  public currentAmount = input.required<number>();
+  public totalAmount = input.required<number>();
 }
