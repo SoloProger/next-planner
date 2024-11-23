b43772660743a4cacab87cfbb73df0af73bf6316
andreisolonnikov <asolonnikow@gmail.com>
2024-11-23 09:07:05 +0300

    dev: Вернул добавление записей

--- client/src/entities/record/record.module.ts
+++ client/src/entities/record/record.module.ts
@@ -3,11 +3,28 @@ import { RecordsHistoryComponent } from './ui/records-history/records-history.co
 import { CommonModule } from '@angular/common';
 import { RecordDetailComponent } from './ui/record-detail/record-detail.component';
 import { TableComponent } from '../../shared/ui/table/table.component';
+import { RecordFormComponent } from './ui/record-form/record-form.component';
+import { DialogService } from '../../shared/ui/dialog/services/dialog.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { InputComponent } from '../../shared/ui/input/input.component';
+import { SelectComponent } from '../../shared/ui/select/ui/select/select.component';
+import { ButtonComponent } from '../../shared/ui/button/button.component';
 
 @NgModule({
-  imports: [CommonModule, TableComponent],
+  imports: [
+    CommonModule,
+    TableComponent,
+    ReactiveFormsModule,
+    InputComponent,
+    SelectComponent,
+    ButtonComponent,
+  ],
   exports: [RecordsHistoryComponent, RecordDetailComponent],
-  declarations: [RecordsHistoryComponent, RecordDetailComponent],
-  providers: [],
+  declarations: [
+    RecordsHistoryComponent,
+    RecordDetailComponent,
+    RecordFormComponent,
+  ],
+  providers: [DialogService],
 })
 export class RecordModule {}
--- client/src/entities/record/ui/records-history/records-history.component.ts
+++ client/src/entities/record/ui/records-history/records-history.component.ts
@@ -1,33 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { Record } from '../../model/types/Record';
 import { BaseStrapiApiService } from '../../../../shared/api/services/base-strapi-api.service';
 import { EntityDataModel } from '../../../../shared/model/types/EntityDataModel';
 import { tableColumns } from '../../constants/tableColumns';
 import { Router } from '@angular/router';
+import { DialogService } from '../../../../shared/ui/dialog/services/dialog.service';
+import { RecordFormComponent } from '../record-form/record-form.component';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-records-history',
   templateUrl: 'records-history.component.html',
 })
 export class RecordsHistoryComponent implements OnInit {
+  public router = inject(Router);
+  public api: BaseStrapiApiService<Record, { data: Record }> =
+    inject(BaseStrapiApiService);
+
+  public dialog = inject(DialogService);
+
   public tableColumns = tableColumns;
 
-  public records$: BehaviorSubject<EntityDataModel<Record>[]> =
-    new BehaviorSubject<EntityDataModel<Record>[]>([]);
-
-  constructor(
-    private router: Router,
-    private apiService: BaseStrapiApiService<Record, Record>
-  ) {}
+  public records = signal<EntityDataModel<Record>[]>([]);
 
   ngOnInit() {
-    this.apiService
+    this.api
       .getAll('records')
-      .subscribe(records => this.records$.next(records.data));
+      .subscribe(records => this.records.set(records.data));
   }
 
   public navigateToDetailPage(item: EntityDataModel<Record>) {
     this.router.navigate([`/detail-record/${item.id}`]);
+  }
+
+  public addNewRecord(): void {
+    this.dialog
+      .openDialog(RecordFormComponent, {
+        title: 'Добавить новую запись',
+      })
+      .afterClosed.pipe(
+        switchMap(formData => this.api.post('records', { data: formData }))
+      )
+      .subscribe(record => {
+        const _record = record as unknown as { data: EntityDataModel<Record> };
+        this.records.update(records => [...records, _record.data]);
+      });
   }
 }
