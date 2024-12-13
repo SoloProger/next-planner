89e0cdf02369b2ac0cbe95d5a02e13e59aafd32d
andreisolonnikov <asolonnikow@gmail.com>
2024-12-13 22:59:54 +0300

    feat/planner-226: редактирование целей

--- client/src/entities/goal/ui/goal-form/goal-form.component.ts
+++ client/src/entities/goal/ui/goal-form/goal-form.component.ts
@@ -1,16 +1,18 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CustomFormBuilder } from '../../../../shared/lib/services/custom-form-builder.service';
 import { Validators } from '@angular/forms';
 import { DialogRef } from '../../../../shared/ui/dialog/lib/utils/dialog-ref';
+import { DialogConfig } from '../../../../shared/ui/dialog/lib/configs/dialog.config';
 
 @Component({
   selector: 'app-goal-form',
   templateUrl: 'goal-form.component.html',
 })
-export class GoalFormComponent {
+export class GoalFormComponent implements OnInit {
   public fb = inject(CustomFormBuilder);
 
   private dialogRef = inject(DialogRef);
+  public dialogData = inject(DialogConfig);
 
   public formGroup = this.fb.group({
     name: ['', Validators.required],
@@ -18,6 +20,10 @@ export class GoalFormComponent {
     currentAmount: ['', Validators.required],
     totalAmount: ['', Validators.required],
   });
+
+  ngOnInit(): void {
+    console.log(this.dialogData);
+  }
 
   public onSubmit(): void {
     this.dialogRef.close(this.formGroup.value);
