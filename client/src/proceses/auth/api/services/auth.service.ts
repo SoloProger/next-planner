ce373c46c77b8e0b2c8aaf567ddaac3f6d5d57c8
andreisolonnikov <asolonnikow@gmail.com>
2024-12-14 18:30:25 +0300

    feat/planner-227: Расширил метод enterAuth

--- client/src/proceses/auth/api/services/auth.service.ts
+++ client/src/proceses/auth/api/services/auth.service.ts
@@ -2,12 +2,17 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment.development';
 import { AuthRequest } from '../../model/types/AuthRequest';
+import { Observable } from 'rxjs';
+import { AuthResponse } from '../../model/types/AuthResponse';
 
 @Injectable()
 export class AuthService {
   private http = inject(HttpClient);
 
-  public login(loginForm: AuthRequest) {
-    return this.http.post(`${environment.apiUrl}/auth/local`, loginForm);
+  public login(loginForm: AuthRequest): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(
+      `${environment.apiUrl}/auth/local`,
+      loginForm
+    );
   }
 }
--- client/src/proceses/auth/ui/login/login-form.component.ts
+++ client/src/proceses/auth/ui/login/login-form.component.ts
@@ -2,6 +2,8 @@ import { Component, inject } from '@angular/core';
 import { CustomFormBuilder } from '../../../../shared/lib/services/custom-form-builder.service';
 import { Validators } from '@angular/forms';
 import { AuthService } from '../../api/services/auth.service';
+import { LocalStorageService } from '../../../../shared/lib/services/localstorage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
@@ -11,6 +13,8 @@ export class LoginFormComponent {
   public form = inject(CustomFormBuilder);
 
   private auth = inject(AuthService);
+  private localStorage = inject(LocalStorageService);
+  private router = inject(Router);
 
   public loginForm = this.form.group({
     login: ['', Validators.required],
@@ -23,6 +27,9 @@ export class LoginFormComponent {
         identifier: this.loginForm.get('login')?.value as string,
         password: this.loginForm.get('password')?.value as string,
       })
-      .subscribe();
+      .subscribe(res => {
+        this.localStorage.setItem('token', res.jwt);
+        this.router.navigate(['/']);
+      });
   }
 }
